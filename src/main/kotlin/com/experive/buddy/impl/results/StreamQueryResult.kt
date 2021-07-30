150f0feed144e18b818b06ff5d9a51d61f488c22
Michele Masili <michele.masili@creactives.com>
2021-07-30 09:45:45 +0200

    Attempt to solve github compilation issues

--- src/main/kotlin/com/experive/buddy/impl/results/StreamQueryResult.kt
+++ src/main/kotlin/com/experive/buddy/impl/results/StreamQueryResult.kt
@@ -1,11 +1,12 @@
 package com.experive.buddy.impl.results
 
 import com.experive.buddy.QueryResult
+import java.util.stream.Collectors
 import java.util.stream.Stream
 
 class StreamQueryResult<E>(private val results: Stream<E>) : QueryResult<E> {
   private val iterator = lazy { results.iterator() }
-  override fun toList(): List<E> = results.toList()
+  override fun toList(): List<E> = results.collect(Collectors.toList())
 
   override fun next(): E = iterator.value.next()
 
